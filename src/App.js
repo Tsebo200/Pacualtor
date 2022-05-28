3fe86b4ce055db7a86d4e61abc9fad2cca349d24
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-05-28 22:17:49 +0200

    Side Profile file created
    Starting with the front-end of the website

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import TaxCalc from './Functions/TaxCalculate';
 import Result from './Functions/Equals';
+import SideProfile from './components/SideProfile';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<TaxCalc/>}/>
+        <Route path="/" element={<SideProfile />} />
+        <Route path="/Tax" element={<TaxCalc />}/>
         <Route path="/savings" element={<Result />}/>
       </Routes>
     </div>
---
+++ src/components/SideProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SideProfile = () => {
+
+    return(
+        <>
+        <div className="main-container">
+        <div className="side-bar-container"></div>
+       
+       
+        </div> 
+        </> 
+    )
+}
+export default SideProfile
