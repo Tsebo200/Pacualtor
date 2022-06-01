80fd79deed9d4bccad7118cf881615cd15fdbe59
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-06-02 01:49:44 +0200

    Styling Elements In Side Bar
    Styled the brand name, styling the slogan

--- src/components/SideProfile.js
+++ src/components/SideProfile.js
@@ -7,6 +7,8 @@ const SideProfile = () => {
         <div className="main-container">
         <div className="side-bar-container">
         <div className="logo-box"></div>
+        <p className="paculate-text"><mark className="P-text">P</mark><mark className="a-text-one">a</mark><mark className="c-text">c</mark><mark className="u-text">u</mark><mark className="l-text">l</mark><mark className="a-text-two">a</mark><mark className="t-text">t</mark><mark className="o-text">o</mark><mark className="r-text">r</mark></p>
+        <p className="slogan"><mark className="your-text">Your</mark><mark className="personal-text">Personal</mark><mark className="budget-text">Budget</mark><mark className="paculator-text">Paculator</mark></p>
         </div>
         <div className="net-income-container"></div>
         <div className="income-after-tax-container"></div>
