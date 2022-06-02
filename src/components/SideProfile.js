e5a0f7b4697b56bf00970d7bf336fdb9079c50ec
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-06-02 06:14:13 +0200

    Completed Style Net Income component

--- src/components/SideProfile.js
+++ src/components/SideProfile.js
@@ -24,9 +24,16 @@ const SideProfile = () => {
         <div className="begin-btn" type={'submit'}><p className="begin-text">Let's Begin</p></div>
         </div>
         {/* End of Side-Bar Container */}
-        
-        <div className="net-income-container">
 
+        <div className="net-income-container">
+            <p className="net-income-text">Total Net Income</p>
+            <div className="net-income-output-container">
+                <div className="rand-container"><p className="R-text">R</p></div>
+                <div className="net-income-input-box"></div>
+            </div>
+            <p className="name-one-net-inc">Name One</p>
+            <div className="line-breaker"></div>
+            <p className="name-two-net-inc">Name Two</p>
         </div>
         {/* End of Net Income Container */}
         <div className="income-after-tax-container"></div>
